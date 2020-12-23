1ccde3a509026495c43a631446693f4c66b8392d
linpakhei <linpakhei@gmail.com>
2020-12-23 17:19:12 +0800

    first commit

---
+++ bot.php
@@ -0,0 +1,20 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    $arrContextOptions = array(
+        "ssl"=>array(
+            "verify_peer"=>false,
+            "verify_peer_name"=>false,
+        ),
+    ); 
+
+    $token = "1429201383:AAG97dhytzYKCBBttkTSSwICRXkJ416pmIk";
+    $apiUrl = "https://api.telegram.org/bot".$token;
+
+    $getUpdates = file_get_contents($apiUrl.'/getUpdates', false, stream_context_create($arrContextOptions));
+
+    $getUpdatesArray = json_decode($getUpdates);
+
+    print_r($getUpdatesArray);
+?>
